228c350b14e7aa2f8a732209dd779df9b8c3a212
Zach Smith <x.coder.zach@gmail.com>
2014-07-26 16:12:55 -0400

    menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,57 @@
-var Physics = require('../../lib')
-Hammer = require('hammerjs')
-var mc = new Hammer.Manager($('.example1 .object')[0]);
-mc.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 }) );
+$(function() {
+  var Physics = require('../../lib')
+  var menuHandle = document.querySelector('.nav-header')
+  var closeHandle = document.querySelector('.close-handle')
+  var menu = document.querySelector('.pull-down-menu')
+  var height = window.innerHeight
 
-var phys = new Physics($('.example1 .object'))
-var opts = {}
+  var phys = new Physics(menu)
+  //only translateY, since the since the menu can't move horizontally
+  phys.style('translateY', function(pos) { return pos.y + 'px' })
 
-phys.style('translateX', function(pos) { return pos.x + 'px' })
-    .style('translatey', function(pos) { return pos.y + 'px'})
+  var interaction
+    , startY
+    , mousedown = false
 
-//set the initial position
-phys.position(50, 50)
+  function start(evt) {
+    mousedown = true
+    interaction = phys.interact()
+    interaction.start()
 
-$('.example1 .animate').on('click', function() {
-  opts = {
-    tension: parseFloat($('.example1 .tension').val()),
-    damping: parseFloat($('.example1 .damping').val()),
+    startY = evt.touches[0].pageY - phys.position().y
   }
-  phys.position(50, 50)
-    .spring(100, 50, 50, 400, 50, opts)
-})
 
-var startPosition
+  function move(evt) {
+    evt.preventDefault()
+    var position = evt.touches[0].pageY - startY
 
-mc.on("panstart", function() {
-  phys.stop()
-  startPosition = phys.position()
-})
+    //ensure the user can't pull the menu down below the bottom of the page
+    position = Math.min(position, height)
+    interaction.position(0, position)
+  }
 
-mc.on('pan', function(evt) {
-  phys.position(
-    startPosition.x + evt.deltaX,
-    startPosition.y + evt.deltaY
-  )
-})
+  function end(evt) {
+    interaction.end()
+    var position = phys.position().y
 
-mc.on('panend', function(evt) {
-  phys.spring(400, 50, opts)
+    // if the menu was moving up when the user released it,
+    // or if they released it with no velocity, and they released in the
+    // top half of the screen.
+    if(phys.direction('up') || (phys.atRest() && position > height / 2)) {
+      phys.spring({ tension: 100, damping: 15 })
+        .to(0, 0).start()
+    } else {
+      phys.accelerate({ acceleration: 1500, bounceAcceleration: 4000, bounce: true })
+        .to(0, height).start()
+    }
+  }
+
+  menuHandle.addEventListener('touchstart', start)
+  menuHandle.addEventListener('touchmove', move)
+  menuHandle.addEventListener('touchend', end)
+
+  closeHandle.addEventListener('touchstart', start)
+  closeHandle.addEventListener('touchmove', move)
+  closeHandle.addEventListener('touchend', end)
+
 })
